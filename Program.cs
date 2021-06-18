c4efb56e81593e5c174772a50e6c476621286e19
Hacer Bayram <hacer.bayram@carl-it.net>
2021-06-18 12:49:50 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HacerBayram
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+        
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Type your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Type your surname:");
+        string surname = Console.ReadLine();
+        Console.WriteLine("Hello "+name + " " +surname);
+
+        }
+        
+
+
+    }
+}
